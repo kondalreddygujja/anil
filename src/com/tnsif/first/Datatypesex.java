2b026c35c385f9338dee166ff05079cf751a4c9b
kondalreddygujja <kondalreddygujja26@gmail.com>
2025-09-14 13:56:54 +0530

    comit

---
+++ src/com/tnsif/first/Datatypesex.java
@@ -0,0 +1,37 @@
+package com.tnsif.first;
+
+public class Datatypesex {
+	   
+	    public static void main(String[] args) {
+	        
+	       
+	        byte b = 100;
+	        short s = 30000;
+	        int i = 123456;
+	        long l = 1234567890L;
+	        float f = 3.14f;
+	        double d = 99.99;
+	        char c = 'A';
+	        boolean flag = true;
+
+	        System.out.println("Primitive Data Types:");
+	        System.out.println("byte: " + b);
+	        System.out.println("short: " + s);
+	        System.out.println("int: " + i);
+	        System.out.println("long: " + l);
+	        System.out.println("float: " + f);
+	        System.out.println("double: " + d);
+	        System.out.println("char: " + c);
+	        System.out.println("boolean: " + flag);
+	        
+	        String str = "Hello Java";  
+	        int[] arr = {1, 2, 3, 4, 5}; 
+
+	        System.out.println("\nNon-Primitive Data Types:");
+	        System.out.println("String: " + str);
+	        System.out.println("Array (example element): " + arr[0]); 
+	    }
+	
+
+
+}
