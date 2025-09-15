ed7879c645843abf48ef464bf48ff70e2882a3c6
kondalreddygujja <kondalreddygujja26@gmail.com>
2025-09-15 11:06:41 +0530

    comit

---
+++ src/com/tnsif/second/Evenexam.java
@@ -0,0 +1,16 @@
+package com.tnsif.second;
+
+public class Evenexam {
+	
+	    public static void main(String[] args) {
+	        int i = 5;  
+	        if (i % 2 == 0) {
+	            System.out.println(i + " is Even");
+	        } else {
+	            System.out.println(i + " is Odd");
+	        }
+	    }
+	}
+
+
+
